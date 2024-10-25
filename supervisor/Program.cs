42ab230cd630d3b55d14e24465e902ff2e13d8fd
fejpet <fejp3t@gmail.com>
2024-10-25 21:07:15 +0200

    first version number

--- supervisor/Program.cs
+++ supervisor/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/version", () => """{"supervisor": "0.1"}""");
 
 app.Run();
